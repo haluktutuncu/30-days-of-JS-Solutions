3d6128f0e0c100190d2015ea5cf03d89c632049d
haluktutuncu <112336855+haluktutuncu@users.noreply.github.com>
2023-08-21 14:51:03 +0300

    first commit

---
+++ 1-Introduction/datatypes.js
@@ -0,0 +1,27 @@
+console.log(typeof string);
+console.log(typeof booboo);
+console.log(typeof und);
+console.log(typeof typeNull);
+
+string;
+Boolean;
+undefined;
+null;
+Number;
+
+/*const firstName = "Haluk";
+const surname = "Tütüncü";
+const maritalStatus = "single";
+let country = "Türkiye";
+let age = 24;*/
+
+const firstName = "Haluk",
+  surname = "Tütüncü",
+  maritalStatus = "single",
+  country = "Türkiye",
+  age = 24;
+
+const myAge = 24,
+  yourAge = 25;
+
+console.log(`I am ${age} years old, you are ${yourAge} years old.`);
---
+++ 1-Introduction/variables.js
@@ -0,0 +1,5 @@
+let string = "String";
+let booboo = true; // true or false
+
+let und = undefined;
+let typeNull = null;
